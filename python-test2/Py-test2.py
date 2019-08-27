4226a86905562edfe0669940835c684a27499f73
Python_test <aiquanpeng@163.com>
2019-08-27 20:37:01 +0800

    python_test1

---
+++ python-test2/Py-test2.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+from urllib import request
+
+url ="https://search.51job.com/list/040000,000000,0000,00,9,99,%25E6%2595%25B0%25E6%258D%25AE%25E6%258C%2596%25E6%258E%2598%25E5%25B7%25A5%25E7%25A8%258B%25E5%25B8%2588,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=4&dibiaoid=0&address=&line=&specialarea=00&from=&welfare="
+head = {}
+head['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+url = request.Request(url,headers= head)
+respone = request.urlopen(url)
+html = respone.read().decode("gbk")
+
+soup = BeautifulSoup(html,"lxml")
